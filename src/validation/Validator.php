ff8d4937de150484ef4b9fcc5e79e53783aef601
Fernando Mangussi <mangussi@gmail.com>
2018-08-03 16:21:45 -0700

    simplified code

--- src/validation/Validator.php
+++ src/validation/Validator.php
@@ -2,52 +2,50 @@
 
 use Respect\Validation\Validator as Valid;
 
-class Validator {
+class Validator
+{
+    public function isValid($validation_data)
+    {
+        $errors = [];
 
-  public function isValid($validation_data)
-  {
-    $errors = [];
+        foreach ($validation_data as $name => $value) {
+            $rules = explode("|", $value);
 
-    foreach ($validation_data as $name => $value) {
+            foreach ($rules as $rule) {
+                // code...
 
-      $rules = explode("|", $value);
-
-      foreach ($rules as $rule) {
-        // code...
-        if(isset($_REQUEST[$name])){
-            $exploded = explode(":", $rule);
-            switch ($exploded[0]) {
+                $exploded = explode(":", $rule);
+                switch ($exploded[0]) {
               case 'min':
                 $min = $exploded[1];
 
-                if(Valid::stringType()->length($min)->Validate($_REQUEST[$name]) == false){
-                  $errors[] = $name . " must be at least " . $min . " characters long!";
+                if (Valid::stringType()->length($min)->Validate($_REQUEST[$name]) == false) {
+                    $errors[] = $name . " must be at least " . $min . " characters long!";
                 }
                 break;
 
               case 'email':
 
-                if(Valid::email()->Validate($_REQUEST[$name]) == false){
-                  $errors[] = $name . " must be a valid email address!";
+                if (Valid::email()->Validate($_REQUEST[$name]) == false) {
+                    $errors[] = $name . " must be a valid email address!";
                 }
                 break;
 
               case 'equalTo':
                 #v::equals('alganet')->validate('alganet'); // true
-                if(Valid::equals($_REQUEST[$name])->Validate($_REQUEST[$exploded[1]]) == false){
-                  $errors[] = $name . " must be equal to " . $exploded[1] ;
+                if (Valid::equals($_REQUEST[$name])->Validate($_REQUEST[$exploded[1]]) == false) {
+                    $errors[] = $name . " must be equal to " . $exploded[1] ;
                 }
                 break;
 
 
               default:
+                // $errors[] = "<br>" . "\$_REQUEST[\$name] => ". $_REQUEST[$name] . "<br>\$rule =>  " . $rule .  "<br>No value found for [" . $exploded[0] . "]";
+                $errors[] = "No value found!";
                 break;
             }
-        } else { //if(isset($_REQUEST[$name])){
-          $errors[] = "<br>" . "\$_REQUEST[\$name] => ". $_REQUEST[$name] . "<br>\$rule =>  " . $rule .  "<br>No value found for [" . $exploded[0] . "]";
-        }//if(isset($_REQUEST[$name])){
-      } //foreach ($rules as $rule) {
-    } // foreach ($validation_data as $name => $value) {
+            } //foreach ($rules as $rule) {
+        } // foreach ($validation_data as $name => $value) {
     return $errors;
-  }
+    }
 }
