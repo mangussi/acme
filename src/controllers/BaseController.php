9a654886567d4ab13c2de40334c105e82a0975e2
Fernando Mangussi <mangussi@gmail.com>
2018-08-03 18:45:09 -0700

    Development
    added database migrations; updated composer.json to use phinx; added
    phinx files

--- src/controllers/BaseController.php
+++ src/controllers/BaseController.php
@@ -14,18 +14,5 @@ class BaseController
       $this->blade = new BladeInstance(__DIR__ . "/../../views", __DIR__ . "/../../views/cache");
     }
 
-    public function getShowHomePage()
-    {
-        // include(__DIR__. "/../../views/home.php");
-        // echo $this->twig->render('home.html');
-        echo $this->blade->render("home");
-    }
-
-    public function getShowLoginPage()
-    {
-        // include(__DIR__. "/../../views/login.html");
-        // echo $this->twig->render('login.html');
-        echo $this->blade->render("login");
-    }
 
 }
--- src/controllers/PageController.php
+++ src/controllers/PageController.php
@@ -5,10 +5,20 @@ use Acme\Models\User;
 
 class PageController extends BaseController
 {
-    public function getShowRegisterPage()
+    public function getShowLoginPage()
     {
-        echo $this->blade->render("register");
+        // include(__DIR__. "/../../views/login.html");
+        // echo $this->twig->render('login.html');
+        echo $this->blade->render("login");
     }
 
+    public function getShowHomePage()
+    {
+        echo $this->blade->render("home");
+    }
 
+    public function getShowPage()
+    {
+        echo "foo!";
+    }
 }
